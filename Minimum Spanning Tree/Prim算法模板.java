12bfabf4e365e884540f64676541ec2390b2fa85
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-06 13:51:17 -0700

    Update Prim算法模板.java

--- Minimum Spanning Tree/Prim算法模板.java
+++ Minimum Spanning Tree/Prim算法模板.java
@@ -1,1 +1,25 @@
-
+public int prim(int n,int[][] edges){
+    boolean[] vis=new boolean[n+1];
+    int[] dist=new int[n+1];
+    int u=1,res=0;
+    vis[u]=true;
+    for(int i=1;i<=n;i++){
+        dist[i]=edges[u][i];
+    }
+    int mmin;
+    for(int i=1;i<=n;i++){
+        mmin=Integer.MAX_VALUE;
+        for(int j=1;j<=n;j++){
+            if(!vis[j]&&mmin>dist[j]){
+                mmin=dist[j];
+                u=j;
+            }
+        }
+        if(mmin==Integer.MAX_VALUE) return res;
+        vis[u]=true;
+        res+=mmin;
+        for(int j=1;j<=n;j++){
+            if(!vis[j]&&dist[j]>edges[u][j]) dist[j]=edges[u][j];
+        }
+    }
+}
