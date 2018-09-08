2ff9c667812a7d2ccb42b666fe5cccabb5891679
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-08 12:38:03 -0700

    Update CF821D. Okabe and City-SPFA.cpp

--- Shortest Path/CF821D. Okabe and City-SPFA.cpp
+++ Shortest Path/CF821D. Okabe and City-SPFA.cpp
@@ -45,3 +45,71 @@ Input
 Output
 0
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);i++)
+#define RREP(i,s,t) for(int i=(s);i>=(t);i--)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=10010;
+struct node{
+    int x,y;
+}mat[N]; //构建点集,无需邻接表因为只能去相邻的四个节点
+int dist[N];
+int vis[N];
+int n,m,k;
+
+void spfa(int u){
+    queue<int> q;
+    q.push(u);
+    dist[u]=0;
+    vis[u]=1;
+    while(!q.empty()){
+        u=q.front();q.pop();
+        vis[u]=0;
+        REP(v,1,k+1){
+            if(u==v) continue;
+            int w=INF,distx=abs(mat[u].x-mat[v].x),disty=abs(mat[u].y-mat[v].y); //检查各点与当前节点的距离
+            if(distx+disty==1) w=0; //相邻则边权为0,无需点亮
+            else if(distx<=2||disty<=2) w=1; //间隔一行或一列则边权为1,需点亮间隔的行或列
+            if(dist[v]>dist[u]+w){
+                dist[v]=dist[u]+w;
+                if(!vis[v]){
+                    q.push(v);
+                    vis[v]=1;
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    int flg=0;
+    scanf("%d%d%d",&n,&m,&k);
+    REP(i,1,k+1){
+        int u,v;
+        scanf("%d%d",&mat[i].x,&mat[i].y);
+        if(mat[i].x==n&&mat[i].y==m) flg=1; //检查终点节点是否点亮
+    }
+    if(!flg){ //若终点(n,m)未点亮,点亮(n+1,m+1)并作为终点节点
+        mat[++k].x=n+1;
+        mat[k].y=m+1;
+    }
+    FILL(dist,0x3f);
+    FILL(vis,0);
+    spfa(1);
+    if(dist[k]==INF) cout<<-1;
+    else cout<<dist[k];
+}
