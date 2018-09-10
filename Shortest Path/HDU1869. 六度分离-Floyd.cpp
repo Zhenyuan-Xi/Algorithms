30c7b1288e89efd4f64ae30ea535073cd83cec0c
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-10 15:19:43 -0700

    Update HDU1869. 六度分离-Floyd.cpp

--- Shortest Path/HDU1869. 六度分离-Floyd.cpp
+++ Shortest Path/HDU1869. 六度分离-Floyd.cpp
@@ -34,3 +34,63 @@ Output
 Yes
 Yes
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=110;
+int n,m;
+int dist[N][N];
+
+void floyd(){
+    REP(k,0,n){
+        REP(i,0,n){
+            REP(j,0,n){
+                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+            }
+        }
+    }
+}
+
+int main(){
+    while(scanf("%d%d",&n,&m)==2){
+        FILL(dist,0x3f);
+        while(m--){
+            int u,v;
+            scanf("%d%d",&u,&v);
+            dist[u][v]=dist[v][u]=1;
+        }
+        floyd();
+        bool flg=false;
+        REP(i,0,n){
+            if(flg) break;
+            REP(j,0,n){
+                if(dist[i][j]>7){flg=true;break;} //求出全源最短路后,检查是否有两点间距离大于7
+            }
+        }
+        if(flg) cout<<"No"<<endl;
+        else cout<<"Yes"<<endl;
+    }
+}
