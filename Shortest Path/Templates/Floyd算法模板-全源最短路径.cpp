c7161666c92a1595b6976fd6aa938b1177209150
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-10 15:53:47 -0700

    Update Floyd算法模板-全源最短路径.cpp

--- Shortest Path/Templates/Floyd算法模板-全源最短路径.cpp
+++ Shortest Path/Templates/Floyd算法模板-全源最短路径.cpp
@@ -23,6 +23,57 @@ typedef pair<int,int> PII;
 const int INF=0x3f3f3f3f;
 const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
 /*----------------------------------------------*/
-int main(){
-    
+const int N=1010;
+int n,m;
+int dist[N][N];
+
+void floyd(){
+    REP(k,1,n+1){ //k作为最外层循环
+        REP(i,1,n+1){
+            REP(j,1,n+1){
+                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]); //floyd求最短路径模板
+            }
+        }
+    }
 }
+
+void print(){ //打印全源最短路径
+    REP(i,1,n+1){
+        REP(j,i+1,n+1){
+            cout<<i<<"--"<<j<<":"<<(dist[i][j]==INF?-1:dist[i][j])<<endl;
+        }
+    }
+    cout<<endl;
+}
+
+int main(){
+    while(~scanf("%d%d",&n,&m)){
+        FILL(dist,0x3f); //初始化距离矩阵
+        while(m--){
+            int u,v,w;
+            scanf("%d%d%d",&u,&v,&w);
+            dist[u][v]=dist[v][u]=w; //初始化无向图
+        }
+        floyd();
+        print();
+    }    
+}
+
+/*
+测试用例
+Input
+3 1
+1 2 3
+3 3
+1 2 5
+2 3 5
+3 1 2
+Output
+1--2:3
+1--3:-1
+2--3:-1
+
+1--2:5
+1--3:2
+2--3:5
+*/
