c718be6ec97d7b6c14a4bb767f82c12c5abbdb50
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-10 16:01:51 -0700

    Update Floyd算法模板-传递闭包.cpp

--- Shortest Path/Templates/Floyd算法模板-传递闭包.cpp
+++ Shortest Path/Templates/Floyd算法模板-传递闭包.cpp
@@ -23,6 +23,71 @@ typedef pair<int,int> PII;
 const int INF=0x3f3f3f3f;
 const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
 /*----------------------------------------------*/
-int main(){
-    
+const int N=1010;
+int n,m;
+int dist[N][N];
+
+void floyd(){
+    REP(k,1,n+1){
+        REP(i,1,n+1){
+            REP(j,1,n+1){
+                dist[i][j]=dist[i][j]||(dist[i][k]&&dist[k][j]); //floyd传递闭包模板
+            }
+        }
+    }
 }
+
+void print(){
+    REP(i,1,n+1){
+        REP(j,1,n+1){
+            if(i==j) continue;
+            cout<<i<<"->"<<j<<":"<<dist[i][j]<<endl; //打印各点连通性
+        }
+    }
+    cout<<endl;
+}
+
+int main(){
+    while(~scanf("%d%d",&n,&m)){
+        FILL(dist,0); //初始化全源不连通
+        while(m--){
+            int u,v;
+            scanf("%d%d",&u,&v);
+            dist[u][v]=1; //有向图中初始化连通的两点
+        }
+        floyd();
+        print();
+    }
+}
+
+/*
+测试用例
+Input
+5 5
+4 3
+4 2
+3 2
+1 2
+2 5
+Output
+1->2:1
+1->3:0
+1->4:0
+1->5:1
+2->1:0
+2->3:0
+2->4:0
+2->5:1
+3->1:0
+3->2:1
+3->4:0
+3->5:1
+4->1:0
+4->2:1
+4->3:1
+4->5:1
+5->1:0
+5->2:0
+5->3:0
+5->4:0
+*/
