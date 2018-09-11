0bfaa40ea12a7ced77113d414c0d68c51365a377
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-11 15:07:15 -0700

    Update LC785. Is Graph Bipartite?.cpp

--- Bipartite Graph/LC785. Is Graph Bipartite?.cpp
+++ Bipartite Graph/LC785. Is Graph Bipartite?.cpp
@@ -25,3 +25,62 @@ The graph looks like this:
 3----2
 We cannot find a way to divide the set of nodes into two independent subsets.
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        int colors[n];
+        FILL(colors,0);
+        for(int i=0;i<n;i++){
+            if(colors[i]==0&&!bfs(graph,colors,i,1)) return false;
+        }
+        return true;
+    }
+    
+    bool bfs(VII& graph,int colors[],int i,int color){
+        queue<int> q;
+        q.push(i);
+        colors[i]=color;
+        while(!q.empty()){
+            int size=q.size();
+            color=-color;
+            REP(i,0,size){
+                int u=q.front();q.pop();
+                for(int v:graph[u]){
+                    if(!colors[v]){
+                        colors[v]=color;
+                        q.push(v);
+                    }else if(colors[v]!=color) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
