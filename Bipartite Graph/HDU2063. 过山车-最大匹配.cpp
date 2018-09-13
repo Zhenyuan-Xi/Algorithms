6a438035bc8b4e64d81893e17e81586253d6a624
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-13 16:58:40 -0700

    Update HDU2063. 过山车-最大匹配.cpp

--- Bipartite Graph/HDU2063. 过山车-最大匹配.cpp
+++ Bipartite Graph/HDU2063. 过山车-最大匹配.cpp
@@ -21,3 +21,83 @@ Input
 Output
 3
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=1010;
+const int M=2010;
+struct edge{
+    int to,next;
+}edges[M];
+int head[N],tot;
+int vis[N];
+int link[N];
+
+void init(){
+    tot=0;
+    FILL(head,-1);
+}
+
+void addEdge(int u,int v){
+    edges[tot].to=v;
+    edges[tot].next=head[u];
+    head[u]=tot++;
+}
+
+bool hungarian(int u){
+    for(int i=head[u];~i;i=edges[i].next){
+        int v=edges[i].to;
+        if(!vis[v]){
+            vis[v]=1;
+            if(!link[v]||hungarian(link[v])){
+                link[v]=u;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    int k,n,m;
+    while(scanf("%d",&k),k){
+        init();
+        scanf("%d%d",&m,&n);
+        while(k--){
+            int u,v;
+            scanf("%d%d",&u,&v);
+            addEdge(u,v+m);
+            addEdge(v+m,u);
+        }
+        int res=0;
+        FILL(link,0);
+        REP(i,1,m+1){
+            FILL(vis,0);
+            if(hungarian(i)) res++;
+        }
+        cout<<res<<endl;
+    }
+}
