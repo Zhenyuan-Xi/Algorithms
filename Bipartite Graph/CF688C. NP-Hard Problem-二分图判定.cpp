f30e2a73910e85b265061331017bd09d19286256
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-12 17:20:47 -0700

    Update CF688C. NP-Hard Problem-二分图判定.cpp

--- Bipartite Graph/CF688C. NP-Hard Problem-二分图判定.cpp
+++ Bipartite Graph/CF688C. NP-Hard Problem-二分图判定.cpp
@@ -39,3 +39,73 @@ Note
 In the first sample, you can give the vertex number 2 to Arya and vertices numbered 1 and 3 to Pari and keep vertex number 4 for yourself 
 In the second sample, there is no way to satisfy both Pari and Arya.
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=100010;
+VI adj[N];
+VI res[2];
+int colors[N];
+bool valid;
+
+void dfs(int u,int color){
+    if(!valid) return;
+    if(colors[u]!=-1){
+        valid=colors[u]==color;
+        return;
+    }
+    colors[u]=color;
+    res[color].push_back(u);
+    REP(i,0,adj[u].size()){
+        dfs(adj[u][i],color^1);
+    }
+}
+
+int main(){
+    int n,m;cin>>n>>m;
+    while(m--){
+        int u,v;
+        scanf("%d%d",&u,&v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    FILL(colors,-1);
+    valid=true;
+    REP(i,1,n+1){
+        if(colors[i]==-1) dfs(i,0);
+    }
+    if(!valid) cout<<-1<<endl;
+    else{
+        cout<<res[0].size()<<endl;
+        REP(i,0,res[0].size())
+            cout<<res[0][i]<<" ";
+        cout<<endl;
+        cout<<res[1].size()<<endl;
+        REP(i,0,res[1].size())
+            cout<<res[1][i]<<" ";
+        cout<<endl;
+    }
+}
