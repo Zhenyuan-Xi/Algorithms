82a6e668cee26fd6a03b4db03097bc97fa1254e7
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-14 22:00:36 -0700

    Update POJ1274. The Perfect Stall-最大匹配.cpp

--- Bipartite Graph/POJ1274. The Perfect Stall-最大匹配.cpp
+++ Bipartite Graph/POJ1274. The Perfect Stall-最大匹配.cpp
@@ -24,3 +24,86 @@ Input
 Output
 4
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=210;
+const int M=40010;
+struct edge{
+    int to,next;
+}edges[M];
+int head[N],tot;
+int vis[N];
+int link[N];
+int n;
+
+void init(){
+    tot=0;
+    FILL(head,-1);
+}
+
+void addEdge(int u,int v){
+    edges[tot].to=v;
+    edges[tot].next=head[u];
+    head[u]=tot++;
+}
+
+bool hungarian(int u){
+    for(int i=head[u];~i;i=edges[i].next){
+        int v=edges[i].to;
+        if(!vis[v]){
+            vis[v]=1;
+            if(!link[v]||hungarian(link[v])){
+                link[v]=u;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    int m;
+    while(~scanf("%d%d",&n,&m)){
+        init();
+        REP(u,1,n+1){
+            int s;
+            scanf("%d",&s);
+            while(s--){
+                int v;
+                scanf("%d",&v);
+                addEdge(u,v); //牛->棚,只需计算最大匹配时的牛的数量所以不需要棚->牛的边
+            }
+        }
+        FILL(link,0);
+        int res=0;
+        REP(i,1,n+1){
+            FILL(vis,0);
+            if(hungarian(i)) res++;
+        }
+        cout<<res<<endl;
+    }
+}
