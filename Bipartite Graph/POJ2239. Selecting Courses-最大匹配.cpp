a67424f3e4051a371b4a3a3549539330bf486e9a
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-14 18:19:10 -0700

    Update POJ2239. Selecting Courses-最大匹配.cpp

--- Bipartite Graph/POJ2239. Selecting Courses-最大匹配.cpp
+++ Bipartite Graph/POJ2239. Selecting Courses-最大匹配.cpp
@@ -25,3 +25,83 @@ Input
 Output
 4
 */
+
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<string.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=310;
+const int M=50010;
+struct edge{
+    int to,next;
+}edges[M];
+int head[N],tot;
+int vis[N];
+int link[N];
+int n;
+
+void init(){
+    tot=0;
+    FILL(head,-1);
+}
+
+void addEdge(int u,int v){
+    edges[tot].to=v;
+    edges[tot].next=head[u];
+    head[u]=tot++;
+}
+
+bool hungarian(int u){
+    for(int i=head[u];~i;i=edges[i].next){
+        int v=edges[i].to;
+        if(!vis[v]){
+            vis[v]=1;
+            if(!link[v]||hungarian(link[v])){
+                link[v]=u;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    while(~scanf("%d",&n)){
+        init();
+        REP(i,1,n+1){
+            int t;
+            scanf("%d",&t);
+            while(t--){
+                int p,q;
+                scanf("%d%d",&p,&q);
+                addEdge(i,(p-1)*12+q); //以课程号和课程时间(需要hash)为两个点集划分二分图,寻找最大匹配
+            }
+        }
+        FILL(link,0);
+        int res=0;
+        REP(i,1,n+1){
+            FILL(vis,0);
+            if(hungarian(i)) res++;
+        }
+        cout<<res<<endl;
+    }
+}
