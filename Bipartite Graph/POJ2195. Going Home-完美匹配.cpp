9346878ba2e7371658409e43f65e76219cf6e1f3
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-18 13:36:29 -0700

    Update POJ2195. Going Home-完美匹配.cpp

--- Bipartite Graph/POJ2195. Going Home-完美匹配.cpp
+++ Bipartite Graph/POJ2195. Going Home-完美匹配.cpp
@@ -38,3 +38,123 @@ Output
 10
 28
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=210;
+char grid[N][N];
+int xx[N],xy[N],yx[N],yy[N]; //存储输入图以及各点之间x轴y轴距离
+
+int mat[N][N];
+int visx[N];
+int visy[N];
+int lx[N];
+int ly[N];
+int link[N];
+int slack[N];
+int nx,ny;
+
+void init(){
+    FILL(link,-1);
+    FILL(ly,0);
+    REP(x,0,nx){
+        lx[x]=-INF;
+        REP(y,0,ny) lx[x]=max(lx[x],mat[x][y]);
+    }
+}
+
+bool find(int x){
+    visx[x]=1;
+    REP(y,0,ny){
+        if(visy[y]) continue;
+        int gap=lx[x]+ly[y]-mat[x][y];
+        if(gap==0){
+            visy[y]=1;
+            if(link[y]==-1||find(link[y])){
+                link[y]=x;
+                return true;
+            }
+        }else slack[y]=min(slack[y],gap);
+    }
+    return false;
+}
+
+int km(){
+    init();
+    REP(x,0,nx){
+        FILL(slack,0x3f);
+        while(1){
+            FILL(visx,0);
+            FILL(visy,0);
+            if(find(x)) break;
+            int d=INF;
+            REP(y,0,ny){
+                if(!visy[y]) d=min(d,slack[y]);
+            }
+            REP(x,0,nx) if(visx[x]) lx[x]-=d;
+            REP(y,0,ny){
+                if(visy[y]) ly[y]+=d;
+                else slack[y]-=d;
+            }
+        }
+    }
+    int res=0;
+    REP(y,0,ny){
+        res+=mat[link[y]][y];
+    }
+    return res;
+}
+
+int main(){
+    int n,m;
+    while(scanf("%d%d",&n,&m)&&n&&m){
+        nx=ny=0;
+        REP(i,0,n) scanf("%s",grid[i]);
+        REP(i,0,n){
+            REP(j,0,m){
+                if(grid[i][j]=='m'){ //统计X点集中的点的x轴y轴位置
+                    xx[nx]=i;
+                    xy[nx]=j;
+                    nx++;
+                }else if(grid[i][j]=='H'){ //统计Y点集中的点的x轴y轴位置
+                    yx[ny]=i;
+                    yy[ny]=j;
+                    ny++;
+                }
+            }
+            REP(x,0,nx){
+                REP(y,0,ny){
+                    mat[x][y]=-(abs(xx[x]-yx[y])+abs(xy[x]-yy[y])); //初始化邻接矩阵,边权为各个x点和y点之间的曼哈顿距离
+                                                                    //由于要求最小权完美匹配,即将边权取负值后用KM算法求最大权完美匹配
+                }
+            }
+        }
+        int res=km();
+        cout<<-res<<endl;
+    }
+}
