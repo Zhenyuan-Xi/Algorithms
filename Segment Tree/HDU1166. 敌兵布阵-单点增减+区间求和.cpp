7ca610dac374eb7151397dbf5ada6916dc99a582
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-21 15:47:31 -0700

    Update HDU1166. 敌兵布阵-单点增减+区间求和.cpp

--- Segment Tree/HDU1166. 敌兵布阵-单点增减+区间求和.cpp
+++ Segment Tree/HDU1166. 敌兵布阵-单点增减+区间求和.cpp
@@ -41,3 +41,84 @@ Case 1:
 33
 59
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=50010;
+struct node{
+    int l,r,sum;
+}segTree[N*3];
+char str[10];
+int n;
+
+void build(int i,int l,int r){
+    segTree[i].l=l;
+    segTree[i].r=r;
+    if(l==r){ //叶节点
+        scanf("%d",&segTree[i].sum);
+        return;
+    }
+    int mid=(l+r)>>1;
+    build(i<<1,l,mid); //建左树
+    build((i<<1)|1,mid+1,r); //建右树
+    segTree[i].sum=segTree[i<<1].sum+segTree[(i<<1)|1].sum; //根节点的和等于左右子树节点的和相加
+}
+
+void update(int i,int idx,int val){
+    segTree[i].sum+=val; //更新根节点
+    if(segTree[i].l==segTree[i].r) return; //叶节点
+    int mid=(segTree[i].l+segTree[i].r)>>1; 
+    if(idx<=mid) update(i<<1,idx,val); //更新左树
+    else update((i<<1)|1,idx,val); //更新右树
+}
+
+int query(int i,int l,int r){
+    if(segTree[i].l==l&&segTree[i].r==r) return segTree[i].sum; //找到当前查询区间
+    int mid=(segTree[i].l+segTree[i].r)>>1;
+    int sum=0;
+    if(l>mid) sum=query((i<<1)|1,l,r); //右树中查询
+    else if(r<=mid) sum=query(i<<1,l,r); //左树中查询
+    else sum=query(i<<1,l,mid)+query((i<<1)|1,mid+1,r); //累加左右子树中的区间
+    return sum;
+}
+
+int main(){
+    int t,x,y;cin>>t;
+    REP(i,1,t+1){
+        scanf("%d",&n);
+        build(1,1,n);
+        printf("Case %d:\n",i);
+        while(scanf("%s",&str)){
+            if(strcmp(str,"End")==0) break;
+            scanf("%d%d",&x,&y);
+            if(strcmp(str,"Add")==0) update(1,x,y);
+            else if(strcmp(str,"Sub")==0) update(1,x,-y);
+            else printf("%d\n",query(1,x,y));
+        }
+    }
+}
