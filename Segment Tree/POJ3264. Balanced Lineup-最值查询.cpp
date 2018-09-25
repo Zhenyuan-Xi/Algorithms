ac0f3096886a54c6da642866db1c6bf3ba785003
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-24 18:56:24 -0700

    Update POJ3264. Balanced Lineup-最值查询.cpp

--- Segment Tree/POJ3264. Balanced Lineup-最值查询.cpp
+++ Segment Tree/POJ3264. Balanced Lineup-最值查询.cpp
@@ -29,3 +29,83 @@ Output
 3
 0
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=50050;
+struct node{
+    int l,r,mmax,mmin;
+}segTree[N<<2];
+int a[N];
+int n;
+int mmax,mmin;
+
+void build(int i,int l,int r){
+    segTree[i].l=l;
+    segTree[i].r=r;
+    if(l==r){
+        segTree[i].mmax=segTree[i].mmin=a[l];
+        return;
+    }
+    int mid=(l+r)>>1;
+    build(i<<1,l,mid);
+    build((i<<1)|1,mid+1,r);
+    segTree[i].mmax=max(segTree[i<<1].mmax,segTree[(i<<1)|1].mmax);
+    segTree[i].mmin=min(segTree[i<<1].mmin,segTree[(i<<1)|1].mmin);
+}
+
+void query(int i,int l,int r){
+    if(segTree[i].mmax<=mmax&&segTree[i].mmin>=mmin) return;
+    if(segTree[i].l==l&&segTree[i].r==r){
+        mmax=max(mmax,segTree[i].mmax);
+        mmin=min(mmin,segTree[i].mmin);
+        return;
+    }
+    int mid=(segTree[i].l+segTree[i].r)>>1;
+    if(r<=mid) query(i<<1,l,r);
+    else if(l>mid) query((i<<1)|1,l,r);
+    else{
+        query(i<<1,l,mid);
+        query((i<<1)|1,mid+1,r);
+    }
+}
+
+int main(){
+    int q;
+    scanf("%d%d",&n,&q);
+    REP(i,1,n+1) scanf("%d",&a[i]);
+    build(1,1,n);
+    while(q--){
+        int a,b;
+        scanf("%d%d",&a,&b);
+        mmax=-INF;
+        mmin=INF;
+        query(1,a,b); //最值查询
+        printf("%d\n",mmax-mmin);
+    }
+}
