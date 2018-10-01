1be81d9f097b1459631413ab2b7c4ed63dde73d3
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-09-30 21:40:08 -0700

    Update HDU4325. Flowers.cpp

--- Binary Index Tree/HDU4325. Flowers.cpp
+++ Binary Index Tree/HDU4325. Flowers.cpp
@@ -31,3 +31,104 @@ Case #2:
 2
 1
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=200020;
+int c[N]; //树状数组C数组
+int a[N],tot; //离散化数组
+int s[N],t[N]; 
+int q[N];
+
+int lowbit(int x){
+    return x&(-x);
+}
+
+void update(int x,int val){
+    for(int i=x;i<=tot;i+=lowbit(i)){
+        c[i]+=val;
+    }
+}
+
+ll query(int x){
+    ll res=0;
+    for(int i=x;i>0;i-=lowbit(i)){
+        res+=c[i];
+    }
+    return res;
+}
+
+int Bin(int target,int tot){ 
+    int l=0,r=tot-1;
+    while(l<=r){
+        int mid=(l+r)>>1;
+        if(a[mid]==target) return mid;
+        else if(a[mid]>target) r=mid-1;
+        else l=mid+1;
+    }
+}
+
+int main(){
+    int T,n,m;
+    scanf("%d",&T);
+    REP(iCase,1,T+1){
+        printf("Case #%d:\n",iCase);
+        scanf("%d%d",&n,&m);
+        FILL(c,0);
+        FILL(a,0);
+        FILL(s,0);
+        FILL(t,0);
+        FILL(q,0);
+        tot=0;
+        REP(i,0,n){
+            scanf("%d%d",&s[i],&t[i]);
+            a[tot++]=s[i];
+            a[tot++]=t[i];
+        }
+        REP(i,0,m){
+            scanf("%d",&q[i]);
+            a[tot++]=q[i];
+        }
+        sort(a,a+tot); //排序
+        int tmp=tot;
+        tot=1;
+        REP(i,1,tmp){
+            if(a[i]!=a[i-1]) a[tot++]=a[i]; //去重
+        }
+        REP(i,0,n){
+            int l=Bin(s[i],tot)+1; //用二分搜索在离散化数组中找到左边界
+            int r=Bin(t[i],tot)+1; //用二分搜索在离散化数组中找到右边界
+            update(l,1); //区间[l...]增加1
+            update(r+1,-1); //区间[r+1...]减去1,即区间[l...r]增加1
+        }
+        REP(i,0,m){
+            int qu=Bin(q[i],tot)+1; //用二分搜索在离散化数组中找到query点
+            printf("%lld\n",query(qu));
+        }
+    }
+}
