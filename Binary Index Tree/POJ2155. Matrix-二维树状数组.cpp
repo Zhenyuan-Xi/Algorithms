64e88b4230b06dd5b617f0a850bebc876b57eb43
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-10-08 16:19:08 -0700

    Update POJ2155. Matrix-二维树状数组.cpp

--- Binary Index Tree/POJ2155. Matrix-二维树状数组.cpp
+++ Binary Index Tree/POJ2155. Matrix-二维树状数组.cpp
@@ -35,3 +35,79 @@ Output
 0
 1
 */
+
+#include <bits/stdc++.h>
+/*
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<string.h>
+*/
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<vector<int> > VII;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+#define REP(i,s,t) for(int i=(s);i<(t);++i)
+#define RREP(i,s,t) for(int i=(s);i>=(t);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define LEN(x) sizeof(x)/sizeof(x[0])
+#define MP(x,y) make_pair(x,y)
+const int INF=0x3f3f3f3f;
+const int dx[]={-1,0,1,0},dy[]={0,-1,0,1}; //i=3-i
+/*----------------------------------------------*/
+const int N=1010;
+int c[N][N];
+int n;
+
+int lowbit(int i){
+    return i&(-i);
+}
+
+void update(int l,int r,int val){
+    for(int i=l;i<=n;i+=lowbit(i))
+        for(int j=r;j<=n;j+=lowbit(j))
+            c[i][j]+=val;
+}
+
+int query(int l,int r){
+    int ans=0;
+    for(int i=l;i>0;i-=lowbit(i))
+        for(int j=r;j>0;j-=lowbit(j))
+            ans+=c[i][j];
+    return ans;
+}
+
+int main(){
+    int X;
+    scanf("%d",&X);
+    REP(iCase,1,X+1){
+        int t;
+        scanf("%d%d",&n,&t);
+        FILL(c,0);
+        char op[10];
+        int x1,x2,y1,y2;
+        while(t--){
+            scanf("%s",op);
+            if(op[0]=='C'){
+                scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+                update(x2+1,y2+1,1); 
+                update(x2+1,y1,1);
+                update(x1,y2+1,1);
+                update(x1,y1,1); //增加反转次数,即目标区域反转次数增加1,其余区域反转次数增加2即未反转
+            }else{
+                scanf("%d%d",&x1,&y1);
+                if(query(x1,y1)%2==0) printf("0\n");
+                else printf("1\n");
+            }
+        }
+        printf("\n");
+    }
+}
