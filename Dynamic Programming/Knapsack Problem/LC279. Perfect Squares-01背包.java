6db376ab255a287b84c99cc068c1721e819cbff2
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-10-08 22:07:56 -0700

    Update LC279. Perfect Squares-01背包.java

--- Dynamic Programming/Knapsack Problem/LC279. Perfect Squares-01背包.java
+++ Dynamic Programming/Knapsack Problem/LC279. Perfect Squares-01背包.java
@@ -11,3 +11,19 @@ Input: n = 13
 Output: 2
 Explanation: 13 = 4 + 9.
 */
+
+class Solution {
+    public int numSquares(int n) {
+      // F(i): 得到i所需要的最少的完全平方数的数量
+      // F(i) = min{F(i), F(i-j*j)}, j*j为小于i的完全平方数
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
