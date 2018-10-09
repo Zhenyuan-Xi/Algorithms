3f9a0ff23a255c9443df50446fd46e381189457c
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-10-09 00:56:00 -0700

    Update LC322. Coin Change-完全背包.java

--- Dynamic Programming/Knapsack Problem/LC322. Coin Change-完全背包.java
+++ Dynamic Programming/Knapsack Problem/LC322. Coin Change-完全背包.java
@@ -15,3 +15,39 @@ Output: -1
 Note:
 You may assume that you have an infinite number of each kind of coin.
 */
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+      // F(i, j): 由前i个元素组成j所需的最小的元素个数
+      // F(i, j) = min{F(i-1, j), F(i, j-nums[i-1])+1} or F(i-1, j)
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int j = 1; j <= amount; j++) dp[0][j] = amount + 1;
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coins[i - 1])
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][amount] == amount + 1 ? -1 : dp[n][amount];
+    }
+}
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coins[i])
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
