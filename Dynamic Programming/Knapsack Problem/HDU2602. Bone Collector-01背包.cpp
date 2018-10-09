06b5bcfedd5aa9a54ee92a344ac770e463c4fb60
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-10-08 21:42:02 -0700

    Create HDU2602. Bone Collector-01背包.cpp

---
+++ Dynamic Programming/Knapsack Problem/HDU2602. Bone Collector-01背包.cpp
@@ -0,0 +1,24 @@
+/*
+Many years ago , in Teddy’s hometown there was a man who was called “Bone Collector”. 
+This man like to collect varies of bones , such as dog’s , cow’s , also he went to the grave …
+The bone collector had a big bag with a volume of V ,and along his trip of collecting there are a lot of bones , 
+obviously , different bone has different value and different volume, now given the each bone’s value along his trip , 
+can you calculate out the maximum of the total value the bone collector can get ?
+
+Input
+The first line contain a integer T , the number of cases.
+Followed by T cases , each case three lines , the first line contain two integer N , V, (N <= 1000 , V <= 1000 )
+representing the number of bones and the volume of his bag. And the second line contain N integers representing the value of each bone. 
+The third line contain N integers representing the volume of each bone.
+Output
+One integer per line representing the maximum of the total value (this number will be less than 231).
+ 
+Sample Input
+1
+5 10
+1 2 3 4 5
+5 4 3 2 1
+ 
+Sample Output
+14
+*/
